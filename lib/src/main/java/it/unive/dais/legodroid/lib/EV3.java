546ef380d778d0c2daf7c9c614ecb51a1c971149
giorgiac98 <giorgiac98.gc@gmail.com>
2019-10-30 11:29:51 +0100

    Added Legodroid Lib

---
+++ lib/src/main/java/it/unive/dais/legodroid/lib/EV3.java
@@ -0,0 +1,404 @@
+package it.unive.dais.legodroid.lib;
+
+import android.annotation.SuppressLint;
+
+import androidx.annotation.NonNull;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.util.Arrays;
+import java.util.concurrent.Callable;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.FutureTask;
+
+import it.unive.dais.legodroid.lib.comm.AsyncChannel;
+import it.unive.dais.legodroid.lib.comm.Bytecode;
+import it.unive.dais.legodroid.lib.comm.Channel;
+import it.unive.dais.legodroid.lib.comm.Const;
+import it.unive.dais.legodroid.lib.comm.Reply;
+import it.unive.dais.legodroid.lib.plugs.GyroSensor;
+import it.unive.dais.legodroid.lib.plugs.LightSensor;
+import it.unive.dais.legodroid.lib.plugs.TachoMotor;
+import it.unive.dais.legodroid.lib.plugs.TouchSensor;
+import it.unive.dais.legodroid.lib.plugs.UltrasonicSensor;
+import it.unive.dais.legodroid.lib.util.Consumer;
+import it.unive.dais.legodroid.lib.util.Function;
+
+/**
+ * Specialized EV3 class with default Api.
+ * Use this commodity class when you do not need to extend the Api class.
+ */
+public class EV3 extends GenEV3<EV3.Api> {
+    /**
+     * Facility constructor. Creates a SpooledAsyncChannel automatically with the given synchrounous channel
+     *
+     * @param ch a synchrounous channel object.
+     */
+    public EV3(@NonNull AsyncChannel ch) {
+        super(ch);
+    }
+
+    /**
+     * Facility constructor. Creates a SpooledAsyncChannel automatically with the given synchrounous channel
+     *
+     * @param ch a synchrounous channel object.
+     */
+    public EV3(@NonNull Channel ch) {
+        super(ch);
+    }
+
+    /**
+     * Run the given callback as the main program for the EV3 brick.
+     *
+     * @param f the function object representing the main code for the EV3 brick.
+     * @throws AlreadyRunningException thrown when a program is already running on the EV3 brick.
+     */
+    public void run(@NonNull Consumer<Api> f) throws AlreadyRunningException {
+        run(f, Api::new);
+
+        try (Api api = new Api(this)) {
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * This enum type represents the 4 physical input ports on the EV3 brick.
+     */
+    public enum InputPort {
+        /**
+         * Input port 1
+         */
+        _1,
+        /**
+         * Input port 2
+         */
+        _2,
+        /**
+         * Input port 3
+         */
+        _3,
+        /**
+         * Input port 4
+         */
+        _4;
+
+        /**
+         * Encode the input port into a byte for use with {@link Api#getPercentValue(byte, int, int, int)} and {@link Api#getSiValue(byte, int, int, int)}.
+         *
+         * @return a byte according to the encoding defined by the GenEV3 Developer Kit Documentation.
+         * @see <a href="https://le-www-live-s.legocdn.com/sc/media/files/ev3-developer-kit/lego%20mindstorms%20ev3%20firmware%20developer%20kit-7be073548547d99f7df59ddfd57c0088.pdf?la=en-us">LEGO Mindstorms GenEV3 Firmware Developer Kit</a>
+         */
+        public byte toByte() {
+            switch (this) {
+                case _1:
+                    return 0;
+                case _2:
+                    return 1;
+                case _3:
+                    return 2;
+                default:
+                    return 3;
+            }
+        }
+
+        @SuppressLint("DefaultLocale")
+        @Override
+        @NonNull
+        public String toString() {
+            return String.format("In/%d", toByte());
+        }
+    }
+
+    /**
+     * This enum type represents the 4 physical output ports on the EV3 brick.
+     */
+    public enum OutputPort {
+        /**
+         * Output port A
+         */
+        A,
+        /**
+         * Output port B
+         */
+        B,
+        /**
+         * Output port C
+         */
+        C,
+        /**
+         * Output port D
+         */
+        D;
+
+        /**
+         * Encode the output port as a bit mask for certain GenEV3 direct commands that require the bitmask format as parameter.
+         *
+         * @return a byte with the bit mask.
+         * @see <a href="http://google.com</a>https://le-www-live-s.legocdn.com/sc/media/files/ev3-developer-kit/lego%20mindstorms%20ev3%20firmware%20developer%20kit-7be073548547d99f7df59ddfd57c0088.pdf?la=en-us">GenEV3 Developer Kit Documentation</a>
+         */
+        public byte toBitmask() {
+            return (byte) (1 << toByte());
+        }
+
+        /**
+         * Encode the output port into a byte for use with {@link Api#getPercentValue(byte, int, int, int)} and {@link Api#getSiValue(byte, int, int, int)}.
+         * Using output ports for receive operations is possible, though a special encoding is needed according to the GenEV3 Developer Kit Documentation - this is provided by this method.
+         *
+         * @return a byte according to the encoding defined by the GenEV3 Developer Kit Documentation.
+         * @see <a href="http://google.com</a>https://le-www-live-s.legocdn.com/sc/media/files/ev3-developer-kit/lego%20mindstorms%20ev3%20firmware%20developer%20kit-7be073548547d99f7df59ddfd57c0088.pdf?la=en-us">GenEV3 Developer Kit Documentation</a>
+         */
+        public byte toByteAsRead() {
+            return (byte) (toByte() | 0x10);
+        }
+
+        /**
+         * Encode the output port into a byte for use with {@link Api#getPercentValue(byte, int, int, int)} and {@link Api#getSiValue(byte, int, int, int)}.
+         *
+         * @return a byte according to the encoding defined by the GenEV3 Developer Kit Documentation.
+         * @see <a href="http://google.com</a>https://le-www-live-s.legocdn.com/sc/media/files/ev3-developer-kit/lego%20mindstorms%20ev3%20firmware%20developer%20kit-7be073548547d99f7df59ddfd57c0088.pdf?la=en-us">GenEV3 Developer Kit Documentation</a>
+         */
+        public byte toByte() {
+            switch (this) {
+                case A:
+                    return 0;
+                case B:
+                    return 1;
+                case C:
+                    return 2;
+                default:
+                    return 3;
+            }
+        }
+
+        @SuppressLint("DefaultLocale")
+        @Override
+        @NonNull
+        public String toString() {
+            return String.format("Out/%s", super.toString());
+        }
+
+        // TODO: implementare il comando opOutput_Test che verifica se le output port sono occupate
+    }
+
+    /**
+     * This inner class contains the operations that can be performed to control the GenEV3 brick.
+     * Accesing sensors, moving motors etc. are operations that can be performed only by calling methods of this class.
+     * Instances of this class cannot be created by calling a constructor: an instance can only be obtained from the argument of the callback passed to method {@link #run(Consumer, Function)}.
+     * Methods offered by this class belong to two categories: sensor and motor methods are high level operations, though users can directly send low-level
+     * commands to the GenEV3 brick by calling {@link #getSiValue(byte, int, int, int)} and {@link #getPercentValue(byte, int, int, int)} methods.
+     *
+     * @see #getSiValue(byte, int, int, int)
+     * @see #getPercentValue(byte, int, int, int)
+     */
+    public static class Api implements AutoCloseable {
+        /**
+         * Public field pointing to the GenEV3 object.
+         */
+        @NonNull
+        public final GenEV3<? extends Api> ev3;
+
+        protected Api(@NonNull GenEV3<? extends Api> ev3) {
+            this.ev3 = ev3;
+        }
+
+        // high level API
+        //
+
+        /**
+         * Access the light sensor of GenEV3.
+         *
+         * @param port the input port where the light sensor is connected to on the brick.
+         * @return an object of type LightSensor.
+         */
+        @NonNull
+        public LightSensor getLightSensor(@NonNull InputPort port) {
+            return new LightSensor(this, port);
+        }
+
+        /**
+         * Access the touch sensor of GenEV3.
+         *
+         * @param port the input port where the touch sensor is connected to on the brick.
+         * @return an object of type TouchSensor.
+         */
+        @NonNull
+        public TouchSensor getTouchSensor(@NonNull InputPort port) {
+            return new TouchSensor(this, port);
+        }
+
+        /**
+         * Access the ultrasonic sensor of GenEV3.
+         *
+         * @param port the input port where the ultrasonic sensor is connected to on the brick.
+         * @return an object of type UltrasonicSensor.
+         */
+        @NonNull
+        public UltrasonicSensor getUltrasonicSensor(@NonNull InputPort port) {
+            return new UltrasonicSensor(this, port);
+        }
+
+        /**
+         * Access the gyroscope sensor of GenEV3.
+         *
+         * @param port the input port where the gyroscope sensor is connected to on the brick.
+         * @return an object of type GyroSensor.
+         */
+        @NonNull
+        public GyroSensor getGyroSensor(@NonNull InputPort port) {
+            return new GyroSensor(this, port);
+        }
+
+        /**
+         * Access the tacho motor of GenEV3.
+         *
+         * @param port the output port where the motor is connected to on the brick.
+         * @return an object of type TachoMotor.
+         */
+        @NonNull
+        public TachoMotor getTachoMotor(@NonNull OutputPort port) {
+            return new TachoMotor(this, port);
+        }
+
+        /**
+         * Play a sound tone on the GenEV3 brick.
+         *
+         * @param volume   volume within the range [0 - 100].
+         * @param freq     frequency in the range [ 250 - 10000 ].
+         * @param duration duration in milliseconds.
+         * @throws IOException thown when communication errors occur.
+         */
+        public void soundTone(int volume, int freq, int duration) throws IOException {
+            Bytecode bc = new Bytecode();
+            bc.addOpCode(Const.SOUND_CONTROL);
+            bc.addOpCode(Const.SOUND_TONE);
+            bc.addParameter((byte) volume);
+            bc.addParameter((short) freq);
+            bc.addParameter((short) duration);
+            ev3.channel.sendNoReply(bc);
+        }
+
+        // mid level API
+        //
+
+        private final Executor executor = Executors.newSingleThreadExecutor();
+
+        @NonNull
+        private Bytecode prefaceGetValue(byte ready, byte port, int type, int mode, int nvalue) throws IOException {
+            Bytecode r = new Bytecode();
+            r.addOpCode(Const.INPUT_DEVICE);
+            r.addOpCode(ready);
+            r.addParameter(Const.LAYER_MASTER);
+            r.addParameter(port);
+            r.addParameter((byte) type);
+            r.addParameter((byte) mode);
+            r.addParameter((byte) nvalue);
+            r.addGlobalIndex((byte) 0x00);
+            return r;
+        }
+
+        /**
+         * Low level method for sending direct commands to the GenEV3 brick.
+         * This method sends the opInput_Device command READY_SI according to the official GenEV3 Developer Kit Documentation.
+         *
+         * @param port   port number.
+         * @param type   type constant as defined in {@link Const}, e.g. {@link Const#EV3_TOUCH} or {@link Const#EV3_COLOR}.
+         * @param mode   mode constant as defined in {@link Const}, e.g. {@link Const#COL_AMBIENT} or {@link Const#GYRO_ANGLE}.
+         * @param nvalue number of values the command expects to return in the result array.
+         * @return a future object containing an array of 32-bit floats whose length is equal to parameter {@code nvalues}.
+         * @throws IOException thrown when communication errors occur.
+         * @see <a href="https://le-www-live-s.legocdn.com/sc/media/files/ev3-developer-kit/lego%20mindstorms%20ev3%20firmware%20developer%20kit-7be073548547d99f7df59ddfd57c0088.pdf?la=en-us">LEGO Mindstorms GenEV3 Firmware Developer Kit</a>
+         */
+        @NonNull
+        public Future<float[]> getSiValue(byte port, int type, int mode, int nvalue) throws IOException {
+            Bytecode bc = prefaceGetValue(Const.READY_SI, port, type, mode, nvalue);
+            Future<Reply> r = ev3.channel.send(4 * nvalue, bc);
+            return execAsync(() -> {
+                Reply reply = r.get();
+                float[] result = new float[nvalue];
+                for (int i = 0; i < nvalue; i++) {
+                    byte[] bData = Arrays.copyOfRange(reply.getData(), 4 * i, 4 * i + 4);
+                    result[i] = ByteBuffer.wrap(bData).order(ByteOrder.LITTLE_ENDIAN).getFloat();
+                }
+                return result;
+            });
+        }
+
+        /**
+         * Low level method for sending direct commands to the GenEV3 brick.
+         * This method sends the opInput_Device command READY_PCT according to the official GenEV3 Developer Kit Documentation.
+         *
+         * @param port   port number.
+         * @param type   type constant as defined in {@link Const}, e.g. {@link Const#EV3_TOUCH} or {@link Const#EV3_COLOR}.
+         * @param mode   mode constant as defined in {@link Const}, e.g. {@link Const#COL_AMBIENT} or {@link Const#GYRO_ANGLE}.
+         * @param nvalue number of values the command expects to return in the result array.
+         * @return a future object containing an array of 16-bit integers whose length is equal to parameter {@code nvalues}.
+         * @throws IOException thrown when communication errors occur.
+         * @see <a href="https://le-www-live-s.legocdn.com/sc/media/files/ev3-developer-kit/lego%20mindstorms%20ev3%20firmware%20developer%20kit-7be073548547d99f7df59ddfd57c0088.pdf?la=en-us">LEGO Mindstorms GenEV3 Firmware Developer Kit</a>
+         */
+        @NonNull
+        public Future<short[]> getPercentValue(byte port, int type, int mode, int nvalue) throws IOException {
+            Bytecode bc = prefaceGetValue(Const.READY_PCT, port, type, mode, nvalue);
+            Future<Reply> fr = ev3.channel.send(2 * nvalue, bc);
+            return execAsync(() -> {
+                Reply r = fr.get();
+                byte[] reply = r.getData();
+                short[] result = new short[nvalue];
+                for (int i = 0; i < nvalue; i++) {
+                    byte[] bData = Arrays.copyOfRange(reply, 2 * i, 2 * i + 2);
+                    result[i] = ByteBuffer.wrap(bData).order(ByteOrder.LITTLE_ENDIAN).getShort();
+                }
+                return result;
+            });
+        }
+
+        /**
+         * Low level method to execute the callback passed as argument within an Android {@link FutureTask}.
+         * The {@link Executor} in charge of the executing the function the is a builtin single-threaded executor.
+         *
+         * @param c   functional object of type {@link Callable}.
+         * @param <T> the return type of the callback.
+         * @return a {@link FutureTask} object hosting the result of type {@code T}.
+         */
+        @NonNull
+        public <T> FutureTask<T> execAsync(@NonNull Callable<T> c) {
+            FutureTask<T> t = new FutureTask<>(c);
+            executor.execute(t);
+            return t;
+        }
+
+        /**
+         * Low level send command with reply.
+         *
+         * @param reservation global reservation for the result in bytes.
+         * @param bc          object of type {@link Bytecode} representing the command to be sent.
+         * @return a {@link Future} object hosting the {@link Reply} object wrapping the reply by GenEV3.
+         * @throws IOException thrown when communication errors occur.
+         * @see <a href="https://le-www-live-s.legocdn.com/sc/media/files/ev3-developer-kit/lego%20mindstorms%20ev3%20firmware%20developer%20kit-7be073548547d99f7df59ddfd57c0088.pdf?la=en-us">LEGO Mindstorms GenEV3 Firmware Developer Kit</a>
+         */
+        @NonNull
+        public Future<Reply> send(int reservation, @NonNull Bytecode bc) throws IOException {
+            return ev3.channel.send(reservation, bc);
+        }
+
+        /**
+         * Low level send command with no reply.
+         *
+         * @param bc object of type {@link Bytecode} representing the command to be sent.
+         * @throws IOException thrown when communication errors occur.
+         * @see <a href="https://le-www-live-s.legocdn.com/sc/media/files/ev3-developer-kit/lego%20mindstorms%20ev3%20firmware%20developer%20kit-7be073548547d99f7df59ddfd57c0088.pdf?la=en-us">LEGO Mindstorms GenEV3 Firmware Developer Kit</a>
+         */
+        public void sendNoReply(@NonNull Bytecode bc) throws IOException {
+            ev3.channel.sendNoReply(bc);
+        }
+
+        @Override
+        public void close() throws Exception {
+
+        }
+    }
+}
---
+++ lib/src/main/java/it/unive/dais/legodroid/lib/comm/BluetoothConnection.java
@@ -0,0 +1,136 @@
+package it.unive.dais.legodroid.lib.comm;
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Objects;
+import java.util.Set;
+import java.util.UUID;
+
+import it.unive.dais.legodroid.lib.util.Prelude;
+
+import static it.unive.dais.legodroid.lib.util.Prelude.ReTAG;
+
+/**
+ * This class implements a {@link Connection} between the Android device and the GenEV3 via the Bluetooth protocol.
+ * Instances of this class do not represent an active connection but rather a factory for creating the actual connection channel.
+ * The type of the channel is the inner class {@link BluetoothChannel}.
+ */
+public class BluetoothConnection implements Connection<BluetoothConnection.BluetoothChannel> {
+    private static final String TAG = ReTAG("BluetoothConnection");
+
+    @NonNull
+    private final String name;
+    private final BluetoothAdapter adapter = BluetoothAdapter.getDefaultAdapter();
+    @Nullable
+    private BluetoothDevice device = null;
+    @Nullable
+    private BluetoothSocket socket = null;
+
+    /**
+     * Create an object given the name of the GenEV3 device, as configured on the brick settings.
+     * @param name the name of the GenEV3 device. LEGO factory settings default to "GenEV3".
+     */
+    public BluetoothConnection(@NonNull String name) {
+        this.name = name;
+    }
+
+    /**
+     * Create a channel for communication.
+     * Multiple calls to this method do not produce multiple channels: only one active channel is supported.
+     * This method internally performs the bluetooth discovery, searching among paired devices for the device whose name has been passed as argument to the constructor.
+     * @apiNote The GenEV3 device must be first paired with the mobile Android device; refer to the device Bluetooth settings for more info.
+     * @return an object of type {@link BluetoothChannel}.
+     * @throws IOException thrown when communication errors occur.
+     */
+    @NonNull
+    @Override
+    public BluetoothChannel connect() throws IOException {
+        if (socket != null && socket.isConnected()) {
+            Log.w(TAG, "bluetooth socket is already connected");
+            return new BluetoothChannel(socket);
+        }
+        if (adapter == null)
+            throw new IOException("bluetooth is not supported");
+        if (!adapter.isEnabled())
+            throw new IOException("bluetooth adapter is not enabled or unavailable");
+        Set<BluetoothDevice> devs = adapter.getBondedDevices();
+        for (BluetoothDevice dev : devs) {
+            if (dev.getName().equals(name)) {
+                device = dev;
+                break;
+            }
+        }
+        if (device == null)
+            throw new IOException("brick not found");
+        socket = device.createRfcommSocketToServiceRecord(UUID.fromString("00001101-0000-1000-8000-00805F9B34FB"));
+        socket.connect();
+        Log.v(TAG, String.format("bluetooth connected successfully to device '%s'", device.getName()));
+        return new BluetoothChannel(socket);
+    }
+
+    /**
+     * This inner non-static class represents an active bluetooth channel through which the two connected devices communicate sending commands and receiving replies.
+     * @see Channel
+     */
+    public class BluetoothChannel implements Channel {
+        private final String TAG = ReTAG(BluetoothConnection.TAG, ".BluetoothChannel");
+        @NonNull
+        private final InputStream in;
+        @NonNull
+        private final OutputStream out;
+
+        private BluetoothChannel(@NonNull BluetoothSocket socket) throws IOException {
+            in = socket.getInputStream();
+            out = socket.getOutputStream();
+        }
+
+        @Override
+        public void send(@NonNull Command p) throws IOException {
+            byte[] a = p.marshal();
+            byte[] l = new byte[]{(byte) (a.length & 0xFF), (byte) ((a.length >> 8) & 0xFF)};
+            byte[] w = Prelude.concat(l, a);
+//            Log.d(TAG, String.format("send: { %s }", Prelude.bytesToHex(w)));
+            out.write(w);
+        }
+
+        @NonNull
+        @Override
+        public Reply receive() throws IOException {
+            byte[] lb = readSized(2);
+            int len = ((lb[1] & 0xff) << 8) | (lb[0] & 0xff);
+//            Log.d(TAG, String.format("receive len = %d", len));
+            return new Reply(readSized(len));
+        }
+
+        @NonNull
+        private byte[] readSized(int size) throws IOException {
+            byte[] r = new byte[size];
+            int off = 0;
+            while (off < size) {
+//                Log.d(TAG, "reading...");
+                off += in.read(r, off, size - off);
+//                Log.d(TAG, String.format("receive: %s", Prelude.bytesToHex(r)));
+            }
+            return r;
+        }
+
+        @Override
+        public void close() {
+            if (socket != null) {
+                Log.v(TAG, String.format("bluetooth disconnected from device '%s'", Objects.requireNonNull(device).getName()));
+                Prelude.trap(socket::close);
+            }
+        }
+    }
+
+
+}
